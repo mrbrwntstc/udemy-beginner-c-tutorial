3627ccf7c7f4d58e88058213279242f89fd97516
Gaurav Bhaskar <87831380+gaurav-bhaskar@users.noreply.github.com>
2021-09-27 12:03:16 -0500

    create first program

---
+++ hello-world/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Gaurav Bhaskar
+ * @brief Print my name
+ * @version 0.1
+ * @date 2021-09-27
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hi, my name is Gaurav Bhaskar.");
+  return 0;
+}
